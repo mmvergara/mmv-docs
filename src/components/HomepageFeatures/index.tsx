b4d3da709633a37881f7b5091e550a0416c873a6
mmvergara <mark.jesusmanabat@gmail.com>
2022-11-28 13:40:16 +0800

    updated landing page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,18 +10,8 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Why Bother?",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-    description: (
-      <>
-        I thought to myself this making this projec could bring me a lot of benefits like practice
-        writing better English, and documenting my work like I'm in a Job Environment.
-      </>
-    ),
-  },
-  {
     title: "Purpose",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    Svg: require("@site/static/img/aim.svg").default,
     description: (
       <>
         To Document my projects on how to use them, how they are structured, to look back on design
@@ -31,14 +21,14 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+    <div className={clsx("col col--4")}>
+      <div className='text--center'>
+        <Svg className={styles.featureSvg} role='img' />
       </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}a</h3>
+      <div className='text--center padding-horiz--md'>
+        <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
@@ -48,8 +38,8 @@ function Feature({title, Svg, description}: FeatureItem) {
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+      <div className='container'>
+        <div className={styles.sectionRow}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
